25d23c88ef29bc07e230da931f7da4852e40006b
Jomerjom <ekenesjoachim@gmail.com>
2024-01-03 14:49:39 +0100

    added 4x project boxes

--- src/components/projectitem.jsx
+++ src/components/projectitem.jsx
@@ -2,7 +2,7 @@ import { CustomButton } from "./button"
 
 export const ProjectItem = ({ img, title, desc }) => {
     return (
-       <div className={`w-80 h-80 bg-cover bg-center flex items-center justify-center rounded-lg`} style={{ backgroundImage: `url(${img})` }}>
+       <div className={`w-80 h-80 bg-cover bg-center flex items-center justify-center rounded-lg m-2`} style={{ backgroundImage: `url(${img})` }}>
               <div className="bg-black bg-opacity-75 w-full h-full rounded-lg flex flex-col justify-center items-center text-white text-center opacity-0 hover:opacity-100 space-y-3">
                         <h1 className="text-3xl flex">{title}</h1>
                         <p className="px-6">{desc}</p>
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,8 +3,15 @@ import { ProjectItem } from "./projectitem";
 export const Projects = () => {
   return (
     <>
-      <div>
-        <ProjectItem img="../kitty2.png" title="Test Project" desc="A javascript mini-game application where you are dodging asteroids with your rocket!"></ProjectItem>
+    <div className="flex flex-col items-center">
+      <div className="flex">
+        <ProjectItem img="../kitty2.png" title="Test Project" desc="A Vite React project component, to show of your skills on your portfolio!"></ProjectItem>
+        <ProjectItem img="../kitty2.png" title="Test Project" desc="A Vite React project component, to show of your skills on your portfolio!"></ProjectItem>
+      </div>
+      <div className="flex">
+      <ProjectItem img="../kitty2.png" title="Test Project" desc="A Vite React project component, to show of your skills on your portfolio!"></ProjectItem>
+        <ProjectItem img="../kitty2.png" title="Test Project" desc="A Vite React project component, to show of your skills on your portfolio!"></ProjectItem>
+      </div>
       </div>
     </>
   );
